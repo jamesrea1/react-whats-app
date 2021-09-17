a5eaeef20ec33f3e1d8cf92738d5265461799f31
jamesrea1 <jamesreadev@outlook.com>
2021-09-17 19:23:18 +0100

    Add msg status double-tick style

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -66,13 +66,29 @@ function Chat({ chat }) {
           </div>
           <div className="mt-0.5 flex justify-between items-center">
             <span className="inline-block text-sm text-black/60 leading-5 overflow-hidden overflow-ellipsis whitespace-nowrap">
-              {lastMsg.text}
+              {lastMsg.sentByMe && <MsgStatus msg={lastMsg} />}
+              <span>
+                {lastMsg.text}
+              </span>
             </span>
             <span className="inline-block ml-4"></span>
           </div>
         </div>
       </div>
     </button>
+  );
+}
+
+function MsgStatus() {
+  return (
+    <div className="w-18 h-18 inline-block align-top mr-0.5 text-[#4fc3f7]">
+      <svg viewBox="0 0 18 18" width="18" height="18">
+        <path
+          fill="currentColor"
+          d="M17.394 5.035l-.57-.444a.434.434 0 0 0-.609.076l-6.39 8.198a.38.38 0 0 1-.577.039l-.427-.388a.381.381 0 0 0-.578.038l-.451.576a.497.497 0 0 0 .043.645l1.575 1.51a.38.38 0 0 0 .577-.039l7.483-9.602a.436.436 0 0 0-.076-.609zm-4.892 0l-.57-.444a.434.434 0 0 0-.609.076l-6.39 8.198a.38.38 0 0 1-.577.039l-2.614-2.556a.435.435 0 0 0-.614.007l-.505.516a.435.435 0 0 0 .007.614l3.887 3.8a.38.38 0 0 0 .577-.039l7.483-9.602a.435.435 0 0 0-.075-.609z"
+        ></path>
+      </svg>
+    </div>
   );
 }
 
